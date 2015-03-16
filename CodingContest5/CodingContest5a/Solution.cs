c8797f38d5e54ff6f23e87a41436ff4220d3e481
Blake Rollins <brollins90@gmail.com>
2015-03-16 17:42:16 -0600

    end of algo 2... almost

---
+++ CodingContest5/CodingContest5a/Solution.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine(); // read from standard input
+        int i = int.Parse(input);
+
+        Console.WriteLine(new Solution().Go(i)); // write to standard output
+
+
+
+        //Console.WriteLine(new Solution().Go(220)); // write to standard output
+        //Console.WriteLine(new Solution().Go(50)); // write to standard output
+        //Console.WriteLine(new Solution().Go(48)); // write to standard output
+    }
+
+    string Go(int input)
+    {
+
+        HashSet<int> set = new HashSet<int>();
+
+        for (int i = 2; i < input + 1; i++)
+        {
+            int one = input / i;
+            int two = one * i;
+
+            if (input == two)
+            {
+                set.Add(one);
+            }
+
+        }
+
+        int sum = set.Sum();
+
+        HashSet<int> set2 = new HashSet<int>();
+
+        for (int i = 2; i < sum + 1; i++)
+        {
+            int one = sum / i;
+            int two = one * i;
+
+            if (sum == two)
+            {
+                set2.Add(one);
+            }
+
+        }
+
+        int sum2 = set2.Sum();
+
+        if (input == sum2)
+        {
+            return sum.ToString();
+        }
+
+        HashSet<int> set3 = new HashSet<int>();
+
+        for (int i = 2; i < input + 1; i++)
+        {
+            int one = input / i;
+            int two = one * i;
+
+            if (input == two)
+            {
+                set3.Add(one);
+            }
+
+        }
+
+        int sum3 = set3.Sum() -1;
+
+        HashSet<int> set4 = new HashSet<int>();
+
+        for (int i = 2; i < sum3 + 1; i++)
+        {
+            int one = sum3 / i;
+            int two = one * i;
+
+            if (sum3 == two)
+            {
+                set4.Add(one);
+            }
+
+        }
+
+        int sum4 = set4.Sum();
+
+        if (input + 1 == sum4)
+        {
+            return sum3.ToString();
+        }
+
+
+
+
+
+
+        return "none";
+    }
+}
---
+++ CodingContest5/CodingContest5b/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        s.fibgen();
+
+
+        string input = Console.ReadLine(); // read from standard input
+        int i = int.Parse(input);
+
+        Console.WriteLine(new Solution().Go(i)); // write to standard output
+
+
+        //Console.WriteLine(s.Go(5)); // write to standard output
+        //Console.WriteLine(s.Go(1)); // write to standard output
+        //Console.WriteLine(s.Go(10)); // write to standard output
+    }
+
+    static List<long> fib = new List<long>();
+    //static HashSet<long> prime = new HashSet<long>();
+
+    string Go(int input)
+    {
+        return fib[input].ToString();
+    }
+
+    bool IsPrime(long l)
+    {
+        double rootl = Math.Sqrt(l);
+        bool p = true;
+        for (long j = 2; j < rootl &&  j < ((long)int.MaxValue * 4) && p; j++)
+        {
+            if (l % j == 0)
+            {
+                p = false;
+            }
+        }
+        return p;
+    }
+
+    private void fibgen()
+    {
+
+        fib.Add(2);
+
+        long pp = 1;
+        long p = 1;
+        for (int i = 3; i < 100; i++)
+        {
+            long one = pp;
+            long two = p;
+            long f = one + two;
+
+            if (IsPrime(f))
+            {
+                fib.Add(f);
+            }
+            pp = p;
+            p = f;
+        }
+    }
+
+}
