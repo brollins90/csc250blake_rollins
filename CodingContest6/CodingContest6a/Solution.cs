6e7b91b3cecef516e9b20ded757dcdd8bbdb9de0
Blake Rollins <brollins90@gmail.com>
2015-03-20 14:06:18 -0600

    done with algo

--- CodingContest6/CodingContest6a/Solution.cs
+++ CodingContest6/CodingContest6a/Solution.cs
@@ -4,9 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Solution
+public class Solution
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+    //    string input = Console.ReadLine(); // read from standard input
+    //    string[] numbers = input.Split(',');
+    //    int sum = 0;
+    //    foreach (string t in numbers)
+    //    {
+    //        sum += int.Parse(t);
+    //    }
+    //    Console.WriteLine(sum / numbers.Length); // write to standard output
+
+        Console.WriteLine(new Solution().Go("10 7 2 2 2 2 2 3")); // 2
+        Console.WriteLine(new Solution().Go("60 59 29 14 6 3")); // -1
+        Console.WriteLine(new Solution().Go("10 1 2 3 4 5 6 7 8 9 10")); // 1
+        Console.WriteLine(new Solution().Go("10 7 2 2 2 2 2 4")); // 4
+    }
+
+    int Go(string s)
+    {
+        //string input = Console.ReadLine(); // read from standard input
+        string[] numbers = s.Split(' ');
+
+        int[] nums = new int[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            nums[i] = int.Parse(numbers[i]);
+        }
+
+
+        return Go2(nums[0], new List<int>(nums.Skip(1)));
+    }
+
+    int Go2(int goal, List<int> nums)
+    {
+        int tempGoal = goal;
+        int leastNumber = -1;
+
+        bool done = false;
+        List<int> tempnums = new List<int>(nums);
+        while (tempnums.Count > 0)
+        {
+            foreach (int t in tempnums)
+            {
+                List<int> templist = new List<int>(tempnums);
+                templist.Remove(t);
+
+                int sum = templist.Sum();
+                if (sum == tempGoal && leastNumber > templist.Count)
+                {
+                    leastNumber = templist.Count;
+                }
+            }
+            tempnums.RemoveAt(0);
+        }
+
+        return leastNumber;
+
+
     }
 }
--- CodingContest6/CodingContest6b/Solution.cs
+++ CodingContest6/CodingContest6b/Solution.cs
@@ -4,9 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Solution
+public class Solution
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        //string input = Console.ReadLine(); // read from standard input
+        //string[] numbers = input.Split(',');
+        //int sum = 0;
+        //foreach (string t in numbers)
+        //{
+        //    sum += int.Parse(t);
+        //}
+        //Console.WriteLine(sum / numbers.Length); // write to standard output
+
+        Console.WriteLine(new Solution().Go("ababa ab ba a abba"));
+        Console.WriteLine(new Solution().Go("banana b ana"));
+        Console.WriteLine(new Solution().Go("greenbatcorn art green bat corn"));
+    }
+
+    bool Go(string s)
+    {
+        string[] parts = s.Split(' ');
+
+        return Go2(parts[0], parts.Skip(1));
+
+    }
+
+    static HashSet<string> set = new HashSet<string>();
+    bool Go2(string s, IEnumerable<string> ss)
+    
+    {
+        string tempWord = buildWord(ss);
+
+        return set.Contains(s);
+    }
+
+    String buildWord(IEnumerable<string> s)
+    {
+        string ret = "";
+        if (s.Count() == 1)
+        {
+            return s.First();
+        }
+        //else
+        foreach (string w in s)
+        {
+            List<string> newList = new List<string>(s);
+            newList.Remove(w);
+            ret += buildWord(newList) + w;
+            set.Add(ret);
+        }
+        return "";
+
     }
 }
